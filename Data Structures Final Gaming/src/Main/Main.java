ba4a5eade555246cc39d8449449b02c40669fd3d
RaiJashan17 <raijashan17@gmail.com>
2023-02-19 19:43:15 -0800

    Changes to model for checkpoint 2 and frames.

--- Data Structures Final Gaming/src/Main/Main.java
+++ Data Structures Final Gaming/src/Main/Main.java
@@ -12,7 +12,7 @@ public class Main{
 	// Fields (Static) below...
 	public static Color custom=new Color(100,40,150);
 	public static Boolean isImageDrawn=false;
-	public static stopWatchX timer=new stopWatchX(50);
+	public static stopWatchX timer=new stopWatchX(25);
 	public static Queue<Vector2D> vecs1=new LinkedList<>();
 	public static Queue<Vector2D> vecs2 = new LinkedList<>();
 	public static Vector2D currentVec = new Vector2D(-100, -100);
@@ -36,12 +36,7 @@ public class Main{
 	/* This is your access to the "game loop" (It is a "callback" method from the Control class (do NOT modify that class!))*/
 	public static void update(Control ctrl) {
 		// TODO: This is where you can code! (Starting code below is just to show you how it works)
-		if(isImageDrawn){
-			//ctrl.addSpriteToFrontBuffer(1025, 500, "f0"); // Add a tester sprite to render list by tag (Remove later! Test only!)
-			ctrl.addSpriteToFrontBuffer(currentVec.getX(), currentVec.getY(), "f1");
-			
-			//ctrl.drawString(1050, 650, "Jashan Rai", custom);
-		}
+		ctrl.addSpriteToFrontBuffer(currentVec.getX(), currentVec.getY(), "f1");
 		//ctrl.drawString(1050, 650, "Jashan Rai", custom);
 		// Test drawing text on screen where you want (Remove later! Test only!)
 		if(timer.isTimeUp()){
